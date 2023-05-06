7b31d737c441fad05acd127751c2713a18f7c9e1
Benja <benjamin.navarrete.z@mail.pucv.cl>
2023-05-06 15:38:24 -0400

    Agregada modificaciones de eslint y prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'next'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'error',
+    'react/display-name': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  globals: {
+    process: true
+  }
+};
