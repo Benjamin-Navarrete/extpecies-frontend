50415fb224b77971cad06f838122843f2628af6b
EdenFly66 <yerkoandresespinozasoto@gmail.com>
2023-06-04 01:20:18 -0400

    icono especie

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,29 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import data from '../assets/especiesCoordenadas.json';
+import L from 'leaflet';
 
-
-function Marcadores(e){
-   //esto si lo imprime, pero los marcadores no, por algun motivo
+function Marcadores(e) {
+  //esto si lo imprime, pero los marcadores no, por algun motivo
   const coord = e.sitio;
   const nomCom = e.nombreComun;
   const nomCie = e.nombreCientifico;
-  console.log(coord,nomCom,nomCie)
-  return(
+  console.log(coord, nomCom, nomCie);
+  return (
     <Marker position={coord}>
       <Popup>
         {nomCom} <br /> {nomCie}
       </Popup>
     </Marker>
-  )
+  );
+}
+
+function getIcons(img) {
+  const myIcon = L.icon({
+    iconUrl: img,
+    iconSize: [50, 50],
+  });
+  return myIcon;
 }
 
 const Map = () => {
@@ -30,9 +38,17 @@ const Map = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {data.forEach(e => {
+      {data.forEach((e) => {
         Marcadores(e);
       })}
+      <Marker position={data[0].sitio} icon={getIcons(data[0].imagen)}>
+        <Popup>
+          {data[0].nombreComun} <br /> {data[0].nombreCientifico}
+        </Popup>
+      </Marker>
+      {
+        //se puede recorrer manualmente en el peor caso escenario, creo que tengo una idea de como hacerlo iterativo
+      }
     </MapContainer>
   );
 };
