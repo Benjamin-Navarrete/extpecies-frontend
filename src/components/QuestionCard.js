c1cf5575d5206a76145474a0ff6614ea1a41bfd7
Benja <benjamin.navarrete.z@mail.pucv.cl>
2023-06-07 19:13:17 -0400

    Mejorado test, alertas, y validacion de respuestas correctas. Corregidos bugs.

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,33 +1,39 @@
-const QuestionCard = ({ index, imageUrl, question, alternatives }) => {
+// Archivo src/components/QuestionCard.js
+import { useState } from 'react';
+
+const QuestionCard = ({ index, question, alternatives, onSelect }) => {
+  // Creamos un estado para guardar la alternativa seleccionada por el usuario
+  const [selected, setSelected] = useState(null);
+
+  // Creamos una función para manejar el cambio de la alternativa seleccionada
+  const handleChange = e => {
+    // Obtenemos el valor de la alternativa seleccionada
+    const value = e.target.value;
+    // Actualizamos el estado con el valor
+    setSelected(value);
+    // Llamamos a la función onSelect que nos pasaron como prop con el valor
+    onSelect(value);
+  };
+
   return (
-    <div className="bg-white shadow overflow-hidden rounded-lg p-6 mb-6">
-      <div className="grid grid-cols-1 sm:grid-cols-4 gap-6 items-center">
-        <div className="col-span-1 sm:col-span-1">
-          <img
-            src={imageUrl}
-            alt={`Pregunta ${index}`}
-            className="w-full h-48 object-cover"
-          />
-        </div>
-        <div className="col-span-3 sm:col-span-3">
-          <h3 className="text-xl font-semibold mb-4">
-            {index}) {question}
-          </h3>
-          {alternatives.map((alternative, i) => (
-            <div key={i} className="flex items-center mb-2">
-              <input
-                className="form-checkbox text-blue-500"
-                type="checkbox"
-                id={`alternative-${index}-${i}`}
-                value={`option${index}-${i}`}
-                name={`question-${index}`}
-              />
-              <label className="ml-2" htmlFor={`alternative-${index}-${i}`}>
-                {alternative}
-              </label>
-            </div>
-          ))}
-        </div>
+    <div className="bg-white shadow-md rounded-lg p-8 m-6">
+      <h2 className="text-lg font-bold mb-2">
+        {index}. {question}
+      </h2>
+      <div className="grid grid-cols gap-2">
+        {alternatives.map((alternative, i) => (
+          <label key={i} className="flex items-center">
+            <input
+              type="radio"
+              name={`question-${index}`}
+              value={alternative[0]}
+              checked={selected === alternative[0]}
+              onChange={handleChange}
+              className="mr-2"
+            />
+            {alternative}
+          </label>
+        ))}
       </div>
     </div>
   );
--- src/pages/test.js
+++ src/pages/test.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import QuestionCard from '@/components/QuestionCard';
 import DefaultLayout from '@/layouts/DefaultLayout';
 import preguntas from '@/data/preguntas.json';
+// Importamos el paquete de sweet alerts
+import Swal from 'sweetalert2';
 
 const Test = () => {
   // Creamos un estado para guardar las respuestas del usuario
@@ -18,32 +20,29 @@ const Test = () => {
 
   // Creamos una función para enviar el test y calcular el puntaje
   const handleSubmit = () => {
-    // Definimos las respuestas correctas
-    const correctas = {
-      1: 'a',
-      2: 'b',
-      3: 'c',
-      4: 'b'
-    };
-
     // Inicializamos una variable para guardar el puntaje
     let puntaje = 0;
 
     // Recorremos las respuestas del usuario y las comparamos con las correctas
     for (let index in respuestas) {
-      if (respuestas[index] === correctas[index]) {
+      if (respuestas[index] === preguntas[index - 1].correcta) {
         // Si la respuesta es correcta, sumamos un punto al puntaje
         puntaje++;
       }
     }
 
-    // Mostramos un modal con el puntaje obtenido
-    alert(`Tu puntaje es ${puntaje} de ${preguntas.length}`);
+    // Mostramos un modal con el puntaje obtenido usando sweet alerts
+    Swal.fire({
+      title: 'Tu puntaje',
+      text: `${puntaje} de ${preguntas.length}`,
+      icon: 'success',
+      confirmButtonText: 'Ok'
+    });
   };
 
   return (
     <DefaultLayout>
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 max-w-5xl">
         {preguntas.map((pregunta, index) => (
           // Usamos el componente QuestionCard para mostrar cada pregunta con sus alternativas
           <QuestionCard
@@ -56,12 +55,14 @@ const Test = () => {
           />
         ))}
 
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleSubmit}
-        >
-          Enviar test
-        </button>
+        <div className="flex justify-end px-5">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSubmit}
+          >
+            Enviar test
+          </button>
+        </div>
       </div>
     </DefaultLayout>
   );
