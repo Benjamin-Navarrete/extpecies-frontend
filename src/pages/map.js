82172bd8a5907cfe131fba0108f9d509a9c80a48
Benja <benjamin.navarrete.z@mail.pucv.cl>
2023-06-09 13:28:05 -0400

    Cargar las especies desde la base de datos.

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,12 +1,12 @@
 // Archivo src\pages\map.js
 import MapLayout from '@/layouts/MapLayout';
-import especies from '@/data/especies600.json';
 import dynamic from 'next/dynamic';
 import DefaultLayout from '@/layouts/DefaultLayout';
+import axios from 'axios';
 
 const Map = dynamic(() => import('../components/Map'), { ssr: false });
 
-function MapPage() {
+function MapPage({ especies }) {
   return (
     <DefaultLayout>
       <MapLayout>
@@ -16,6 +16,19 @@ function MapPage() {
       </MapLayout>
     </DefaultLayout>
   );
+}
+
+export async function getServerSideProps() {
+  let especies = [];
+  try {
+    const response = await axios.get('http://localhost:3500/api/especies');
+    especies = response.data;
+  } catch (error) {
+    console.error('Error obteniendo las especies: ', error);
+  }
+
+  console.log('Especies: ', especies);
+  return { props: { especies } };
 }
 
 export default MapPage;
