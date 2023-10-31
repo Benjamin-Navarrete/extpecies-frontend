fa2eb1fdf0111440d9b2871577efa76ff65fa585
BenjaPC\Benja <benjamin.navarrete.z@mail.pucv.cl>
2023-10-31 17:57:42 -0300

    Se descarta codigo innecesario de modal de especies

--- src/components/Modals/SpeciesModal.js
+++ src/components/Modals/SpeciesModal.js
@@ -1,78 +1,13 @@
 // src/components/SpeciesModal.js
-import { Dialog, Transition, Listbox } from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useEffect, useState } from 'react';
 import ListModal from './ListModal';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
 import Comment from '../Comentario';
-
 import { createComment, getCommentsByEspecie } from '@/api/commentApi';
-
 import { getLikeByUserAndEspecie } from '@/api/likeApi';
-
-import {
-  FaceSmileIcon as FaceSmileIconOutline,
-  PaperClipIcon
-} from '@heroicons/react/24/outline';
-
-import {
-  FaceFrownIcon,
-  FaceSmileIcon as FaceSmileIconMini,
-  FireIcon,
-  HandThumbUpIcon,
-  HeartIcon,
-  XMarkIcon
-} from '@heroicons/react/20/solid';
 import SpeciesActions from '../SpeciesActions';
-
-const moods = [
-  {
-    name: 'Excited',
-    value: 'excited',
-    icon: FireIcon,
-    iconColor: 'text-white',
-    bgColor: 'bg-red-500'
-  },
-  {
-    name: 'Loved',
-    value: 'loved',
-    icon: HeartIcon,
-    iconColor: 'text-white',
-    bgColor: 'bg-pink-400'
-  },
-  {
-    name: 'Happy',
-    value: 'happy',
-    icon: FaceSmileIconMini,
-    iconColor: 'text-white',
-    bgColor: 'bg-green-400'
-  },
-  {
-    name: 'Sad',
-    value: 'sad',
-    icon: FaceFrownIcon,
-    iconColor: 'text-white',
-    bgColor: 'bg-yellow-400'
-  },
-  {
-    name: 'Thumbsy',
-    value: 'thumbsy',
-    icon: HandThumbUpIcon,
-    iconColor: 'text-white',
-    bgColor: 'bg-blue-500'
-  },
-  {
-    name: 'I feel nothing',
-    value: null,
-    icon: XMarkIcon,
-    iconColor: 'text-gray-400',
-    bgColor: 'bg-transparent'
-  }
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
 
 const SpeciesModal = ({ isOpen, closeModal, especie = {} }) => {
   const {
@@ -89,7 +24,6 @@ const SpeciesModal = ({ isOpen, closeModal, especie = {} }) => {
 
   // Este estado controla si el modal de crear lista está abierto o cerrado
   const [isListModalOpen, setIsListModalOpen] = useState(false);
-  const [selected, setSelected] = useState(moods[5]);
   const [liked, setLiked] = useState(false);
   const [added, setAdded] = useState(false);
   // Estado para el contenido del nuevo comentario
@@ -242,14 +176,6 @@ const SpeciesModal = ({ isOpen, closeModal, especie = {} }) => {
                                   </p>
                                 </div>
                               </div>
-                              {/* <div className="mt-5 flex justify-center sm:mt-0">
-                                <a
-                                  href="#"
-                                  className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
-                                >
-                                  View profile
-                                </a>
-                              </div> */}
                             </div>
                           </div>
                         </div>
@@ -362,126 +288,13 @@ const SpeciesModal = ({ isOpen, closeModal, especie = {} }) => {
                                   onChange={e => setContent(e.target.value)}
                                 />
                               </div>
-                              <div className="flex justify-between pt-2">
-                                <div className="flex items-center space-x-5">
-                                  <div className="flow-root">
-                                    <button
-                                      type="button"
-                                      className="-m-2 inline-flex h-10 w-10 items-center justify-center rounded-full text-gray-400 hover:text-gray-500"
-                                    >
-                                      <PaperClipIcon
-                                        className="h-6 w-6"
-                                        aria-hidden="true"
-                                      />
-                                      <span className="sr-only">
-                                        Attach a file
-                                      </span>
-                                    </button>
-                                  </div>
-                                  <div className="flow-root">
-                                    <Listbox
-                                      value={selected}
-                                      onChange={setSelected}
-                                    >
-                                      {({ open }) => (
-                                        <>
-                                          <Listbox.Label className="sr-only">
-                                            {' '}
-                                            Your mood{' '}
-                                          </Listbox.Label>
-                                          <div className="relative">
-                                            <Listbox.Button className="relative -m-2 inline-flex h-10 w-10 items-center justify-center rounded-full text-gray-400 hover:text-gray-500">
-                                              <span className="flex items-center justify-center">
-                                                {selected.value === null ? (
-                                                  <span>
-                                                    <FaceSmileIconOutline
-                                                      className="h-6 w-6 flex-shrink-0"
-                                                      aria-hidden="true"
-                                                    />
-                                                    <span className="sr-only">
-                                                      {' '}
-                                                      Add your mood{' '}
-                                                    </span>
-                                                  </span>
-                                                ) : (
-                                                  <span>
-                                                    <span
-                                                      className={classNames(
-                                                        selected.bgColor,
-                                                        'flex h-8 w-8 items-center justify-center rounded-full'
-                                                      )}
-                                                    >
-                                                      <selected.icon
-                                                        className="h-5 w-5 flex-shrink-0 text-white"
-                                                        aria-hidden="true"
-                                                      />
-                                                    </span>
-                                                    <span className="sr-only">
-                                                      {selected.name}
-                                                    </span>
-                                                  </span>
-                                                )}
-                                              </span>
-                                            </Listbox.Button>
-
-                                            <Transition
-                                              show={open}
-                                              as={Fragment}
-                                              leave="transition ease-in duration-100"
-                                              leaveFrom="opacity-100"
-                                              leaveTo="opacity-0"
-                                            >
-                                              <Listbox.Options className="absolute z-10 -ml-6 w-60 rounded-lg bg-white py-3 text-base shadow ring-1 ring-black ring-opacity-5 focus:outline-none sm:ml-auto sm:w-64 sm:text-sm">
-                                                {moods.map(mood => (
-                                                  <Listbox.Option
-                                                    key={mood.value}
-                                                    className={({ active }) =>
-                                                      classNames(
-                                                        active
-                                                          ? 'bg-gray-100'
-                                                          : 'bg-white',
-                                                        'relative cursor-default select-none py-2 px-3'
-                                                      )
-                                                    }
-                                                    value={mood}
-                                                  >
-                                                    <div className="flex items-center">
-                                                      <div
-                                                        className={classNames(
-                                                          mood.bgColor,
-                                                          'w-8 h-8 rounded-full flex items-center justify-center'
-                                                        )}
-                                                      >
-                                                        <mood.icon
-                                                          className={classNames(
-                                                            mood.iconColor,
-                                                            'flex-shrink-0 h-5 w-5'
-                                                          )}
-                                                          aria-hidden="true"
-                                                        />
-                                                      </div>
-                                                      <span className="ml-3 block truncate font-medium">
-                                                        {mood.name}
-                                                      </span>
-                                                    </div>
-                                                  </Listbox.Option>
-                                                ))}
-                                              </Listbox.Options>
-                                            </Transition>
-                                          </div>
-                                        </>
-                                      )}
-                                    </Listbox>
-                                  </div>
-                                </div>
-                                <div className="flex-shrink-0">
-                                  <button
-                                    type="submit"
-                                    className="inline-flex items-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
-                                  >
-                                    Comentar
-                                  </button>
-                                </div>
+                              <div className="flex justify-end pt-2">
+                                <button
+                                  type="submit"
+                                  className="inline-flex items-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+                                >
+                                  Comentar
+                                </button>
                               </div>
                             </form>
                           </div>
