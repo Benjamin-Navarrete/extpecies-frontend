31c76351c020b75fd2b7b91181eff689d71fff43
GabrielMedinaS <gabriel.medina.s@mail.pucv.cl>
2023-11-24 01:08:50 -0300

    arreglado bug en gestionar especies

--- src/components/Inputs/InputField.js
+++ src/components/Inputs/InputField.js
@@ -21,7 +21,6 @@ const InputField = ({ label, type, name, placeholder, autoComplete }) => {
           name={name}
           type={type}
           autoComplete={autoComplete}
-          required
           className={`block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-emerald-500 sm:text-sm ${
             meta.touched && meta.error && 'border-red-500'
           }`}
--- src/components/Inputs/TextArea.js
+++ src/components/Inputs/TextArea.js
@@ -21,7 +21,6 @@ const TextArea = ({ label, type, name, placeholder, autoComplete }) => {
           name={name}
           type={type}
           autoComplete={autoComplete}
-          required
           rows="1"
           class={`block w-full h-24 appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-emerald-500 focus:outline-none focus:ring-emerald-500 sm:text-sm ${
             meta.touched && meta.error && 'border-red-500'
